eb7b2402c39f91f8a2783052071ed70d62aac957
이경현 <upstar9898@gmail.com>
2026-04-15 16:32:49 +0900

    fix, refactor: utils.js로 자주 쓰는 함수 분리, service2.html이 직접 script를 쓰지 않고 service2.js를 받아오도록 변경

---
+++ static/bus/js/utils.js
@@ -0,0 +1,38 @@
+export function getCookie(name) {
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== "") {
+        const cookies = document.cookie.split(";");
+        for (let cookie of cookies) {
+            cookie = cookie.trim();
+            if (cookie.startsWith(name + "=")) {
+                cookieValue = decodeURIComponent(cookie.slice(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+
+export function getNowForDateTimeLocal() {
+    const now = new Date();
+
+    const yyyy = now.getFullYear();
+    const MM = String(now.getMonth() + 1).padStart(2, "0");
+    const dd = String(now.getDate()).padStart(2, "0");
+    const hh = String(now.getHours()).padStart(2, "0");
+    const mm = String(now.getMinutes()).padStart(2, "0");
+
+    return `${yyyy}-${MM}-${dd}T${hh}:${mm}`;
+}
+
+export function formatDateTime(dateStr) {
+    const date = new Date(dateStr);
+
+    const yyyy = date.getFullYear();
+    const MM = String(date.getMonth() + 1).padStart(2, "0");
+    const dd = String(date.getDate()).padStart(2, "0");
+    const hh = String(date.getHours()).padStart(2, "0");
+    const mm = String(date.getMinutes()).padStart(2, "0");
+
+    return `${yyyy}-${MM}-${dd} ${hh}:${mm}`;
+}
