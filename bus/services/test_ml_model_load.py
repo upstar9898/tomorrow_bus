d966224f725f03b0fbefd65cadd0dc5ac1f969b9
이경현 <upstar9898@gmail.com>
2026-04-17 14:10:15 +0900

    test : 모델 2개 필요하여 2개만 있는 것으로 수정

--- bus/services/test_ml_model_load.py
+++ bus/services/test_ml_model_load.py
@@ -6,13 +6,9 @@ from django.conf import settings
 
 MODEL1_PATH = os.path.join(settings.BASE_DIR, "ml_models", "test_model1.keras")
 MODEL2_PATH = os.path.join(settings.BASE_DIR, "ml_models", "test_model2.keras")
-MODEL3_PATH = os.path.join(settings.BASE_DIR, "ml_models", "test_model3.keras")
 print("loading ML model1...")
 ml_model_1 = keras.models.load_model(MODEL1_PATH)
 print("loading done.")
 print("loading ML model2...")
 ml_model_2 = keras.models.load_model(MODEL2_PATH)
-print("loading done.")
-print("loading ML model3...")
-ml_model_3 = keras.models.load_model(MODEL3_PATH)
 print("loading done.")
