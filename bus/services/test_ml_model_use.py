5bdd6ba4f5db46de9b457192cc60ca0b62f680df
이경현 <upstar9898@gmail.com>
2026-04-17 14:21:45 +0900

    test : 모델 2개짜리로 변경

--- bus/services/test_ml_model_use.py
+++ bus/services/test_ml_model_use.py
@@ -1,4 +1,4 @@
-from .test_ml_model_load import ml_model1, ml_model2, ml_model3
+from .test_ml_model_load import ml_model1, ml_model2
 from .test_ml_model_preprocess import preprocess
 
 def predict(input_json):
@@ -6,5 +6,5 @@ def predict(input_json):
     station_id = input_json.station_id
     date_time = input_json.date_time
     x = preprocess(input_json)
-    y = [ml_model1.predict(x), ml_model2.predict(x), ml_model3.predict(x)]
+    y = [ml_model1.predict(x), ml_model2.predict(x)]
     return y
